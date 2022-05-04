d2c12c02d70660b51c2abfbe72462bce82c6c7e3
Tellyfun <101307401+Tellyfun@users.noreply.github.com>
2022-05-04 10:51:29 +0530

    Update settings.py

--- plugins/settings/settings.py
+++ plugins/settings/settings.py
@@ -5,39 +5,23 @@ from pyrogram import types, errors
 from plugins.config import Config
 from plugins.database.database import db
 
-async def OpenSettings(m: "types.Message"):
-    usr_id = m.chat.id
-    user_data = await db.get_user_data(usr_id)
-    if not user_data:
-        await m.edit("Failed to fetch your data from database!")
-        return
-    upload_as_doc = user_data.get("upload_as_doc", False)
-    caption = user_data.get("caption", None)
-    apply_caption = user_data.get("apply_caption", True)
-    thumbnail = user_data.get("thumbnail", None)
-    buttons_markup = [
-        [types.InlineKeyboardButton(f"ᴜᴘʟᴏᴀᴅ ᴀs {'🎥 ᴠɪᴅᴇᴏ' if upload_as_doc else '🗃️ ғɪʟᴇ'}",
-                                    callback_data="triggerUploadMode")],
-        [types.InlineKeyboardButton(f"{'ᴄʜᴀɴɢᴇ' if thumbnail else '🌃 sᴇᴛ'} ᴛʜᴜᴍʙɴᴀɪʟ",
-                                    callback_data="setThumbnail")]
-    ]
-    if thumbnail:
-        buttons_markup.append([types.InlineKeyboardButton("🌆 sʜᴏᴡ ᴛʜᴜᴍʙɴᴀɪʟ",
-                                                          callback_data="showThumbnail")])
-    buttons_markup.append([types.InlineKeyboardButton("♨️ ᴄʟᴏsᴇ",
-                                                      callback_data="close")])
-
+async def OpenSettings(m: Message, user_id: int):
     try:
         await m.edit(
-            text="**ʜᴇʀᴇ ʏᴏᴜ ᴄᴀɴ sᴇᴛᴜᴘ ʏᴏᴜʀ sᴇᴛᴛɪɴɢs**",
-            reply_markup=types.InlineKeyboardMarkup(buttons_markup),
-            disable_web_page_preview=True
+            text="Here You Can Change or Configure Your Settings:",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton(f"Upload as {'Video' if (await db.get_upload_as_doc(id=user_id)) is False else 'Document'} ✅", callback_data="triggerUploadMode")],
+                    [InlineKeyboardButton("Show Thumbnail", callback_data="showThumbnail")],
+                    [InlineKeyboardButton("Close", callback_data="close")]
+                ]
+            )
         )
-    except errors.MessageNotModified: pass
-    except errors.FloodWait as e:
+    except MessageNotModified:
+        pass
+    except FloodWait as e:
         await asyncio.sleep(e.x)
-        await show_settings(m)
+        await m.edit("You Are Spamming!")
     except Exception as err:
-        Config.LOGGER.getLogger(__name__).error(err)
-
+        raise err
 
