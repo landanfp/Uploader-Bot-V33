d6b9227feb12dcd34059767e561c762f553b78aa
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-19 13:29:23 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Tellyfun/Uploader-Bot-V3 /Uploader-Bot-V3
-cd /Uploader-Bot-V3
+git clone https://github.com/landanfp/Uploader-Bot-V33 /Uploader-Bot-V33
+cd /Uploader-Bot-V33
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
 python3 bot.py
